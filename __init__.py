a0d1fa3b1c7bf0d9904088ef27765c3ff9591047
`Mr-IoT` <veeru.rockstar249@gmail.com>
2026-03-18 21:41:33 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""ExPoser - Android APK Security Analyzer."""
+
+__version__ = "1.0.0"
+__author__ = "ExPoser Team"
+__description__ = "Android APK Security Analyzer for exported component vulnerabilities"
